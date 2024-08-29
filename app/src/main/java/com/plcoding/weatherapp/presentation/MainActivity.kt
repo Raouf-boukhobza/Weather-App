ba0e0d69d412b9a208f2dc8fdf200cce0fb574a7
raouf <raoufboukhobza3@gmail.com>
2024-08-29 15:30:05 +0100

    add the weather card

--- app/src/main/java/com/plcoding/weatherapp/presentation/MainActivity.kt
+++ app/src/main/java/com/plcoding/weatherapp/presentation/MainActivity.kt
@@ -1,16 +1,40 @@
 package com.plcoding.weatherapp.presentation
 
+import android.Manifest
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.ActivityResultRegistry
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.activity.viewModels
+import com.plcoding.weatherapp.presentation.ui.theme.DeepBlue
 import com.plcoding.weatherapp.presentation.ui.theme.WeatherAppTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+    private val viewModel: WeatherViewModel by viewModels()
+    private lateinit var launchPermission: ActivityResultLauncher<Array<String>>
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+
+        launchPermission = registerForActivityResult(
+            ActivityResultContracts.RequestMultiplePermissions()
+        ) {
+            viewModel.loadWeatherInfo()
+        }
+        launchPermission.launch(
+            arrayOf(
+                Manifest.permission.ACCESS_FINE_LOCATION,
+                Manifest.permission.ACCESS_COARSE_LOCATION,
+            )
+        )
         setContent {
             WeatherAppTheme {
-                
+              WeatherCard(state = viewModel.weatherState, backgroundColor = DeepBlue)
             }
         }
     }
--- app/src/main/java/com/plcoding/weatherapp/presentation/WeatherCard.kt
+++ app/src/main/java/com/plcoding/weatherapp/presentation/WeatherCard.kt
@@ -1,2 +1,92 @@
 package com.plcoding.weatherapp.presentation
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Card
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.vectorResource
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.plcoding.weatherapp.R
+import java.time.format.DateTimeFormatter
+
+
+@Composable
+fun WeatherCard(
+    state: WeatherState,
+    backgroundColor: Color
+) {
+    state.weatherInfo?.currentWeatherData?.let { data ->
+        Card(
+            backgroundColor = backgroundColor,
+            shape = RoundedCornerShape(16.dp),
+            modifier = Modifier.padding(16.dp)
+        ) {
+
+            Column(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(16.dp),
+                verticalArrangement = Arrangement.spacedBy(16.dp),
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Text(
+                    text = "Today ${
+                        data.time.format(DateTimeFormatter.ofPattern("HH:MM"))
+                    }",
+                    fontSize = 16.sp,
+                    modifier = Modifier.align(Alignment.End),
+                    color = Color.White
+                )
+
+                Image(
+                    painter = painterResource(id = data.weatherType.iconRes),
+                    contentDescription = null,
+                    modifier = Modifier.width(200.dp)
+                )
+
+                Text(text = "${data.temperatureCelsius}°C" ,
+                    fontSize = 50.sp,
+                    color = Color.White
+                )
+                Text(text = data.weatherType.weatherDesc,
+                    fontSize = 22.sp,
+                    color = Color.White)
+                Spacer(modifier = Modifier.height(8.dp))
+
+                Row(modifier = Modifier.fillMaxWidth() , 
+                    horizontalArrangement = Arrangement.SpaceBetween){
+                    WeatherDataDisplay(
+                        value = data.windSpeed.toString(),
+                        unit = "Km/h",
+                        icon = ImageVector.vectorResource(id = R.drawable.ic_wind)
+                    )
+                    WeatherDataDisplay(
+                        value = data.humidity.toString(),
+                        unit = "%",
+                        icon = ImageVector.vectorResource(id = R.drawable.ic_drop)
+                    )
+                    WeatherDataDisplay(
+                        value = data.pressure.toString(),
+                        unit = "hpa",
+                        icon = ImageVector.vectorResource(id = R.drawable.ic_pressure)
+                    )
+                }
+            }
+        }
+    }
+}
